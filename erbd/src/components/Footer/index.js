09c3bed4993b267086fa17eec8b0cac0367b0a2c
Chisoo Song <cltn39@gmail.com>
2021-01-03 16:51:26 -0800

    Adjusted Footer text

--- erbd/src/components/Footer/index.js
+++ erbd/src/components/Footer/index.js
@@ -4,7 +4,12 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Made with lots of ❤️ by LumiaDB 2021©</span>
+      <span>
+        Made with lots of ❤️ by LumiaDB 2021©
+      </span>
+      <p>Eternal Return: Black Survival and
+        all related logos are trademarks of Nimble Neuron, inc. or its
+        affiliates.</p>
     </footer>
   );
 }
